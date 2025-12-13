ba4f36daf747bd52d071116f6a2167dfcfd9113d
Dubry <dubrywr@gmail.com>
2025-12-13 17:02:16 +0100

    Added test projects

---
+++ UserManagementService.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace UserManagementService.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ UserManagementService.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace UserManagementService.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
