ed1f38c8bb62e048cf23281624e5d50ac7dac22b
Dubry <dubrywr@gmail.com>
2025-12-13 15:13:40 +0100

    User service (BL) Changed passwordHash on User model to Password

---
+++ UserManagementService/Services/UserService.cs
@@ -0,0 +1,31 @@
+﻿using UserManagementService.Data;
+using UserManagementService.Models;
+
+namespace UserManagementService.Services
+{
+    public class UserService
+    {
+        private readonly AppDbContext _context;
+        private readonly PasswordService _passwordService;
+
+        public UserService(AppDbContext context, PasswordService passwordService)
+        {
+            _context = context;
+            _passwordService = passwordService;
+        }
+
+        public User CreateUser(User user, string password)
+        {
+            user.Password = _passwordService.HashPassword(password);
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return user;
+        }
+
+        public bool ValidatePassword(Guid userId, string password)
+        {
+            var user = _context.Users.Find(userId);
+            return user != null && _passwordService.VerifyPassword(password, user.Password);
+        }
+    }
+}
