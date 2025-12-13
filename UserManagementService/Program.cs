cb0663b14d8d77cd317c118a2592e704c19d5939
Dubry <dubrywr@gmail.com>
2025-12-13 15:08:59 +0100

    Password handling

--- UserManagementService/Program.cs
+++ UserManagementService/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using UserManagementService.Data;
+using UserManagementService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddScoped<PasswordService>();
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
---
+++ UserManagementService/Services/PasswordService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.Security.Cryptography;
+
+namespace UserManagementService.Services
+{
+    public class PasswordService
+    {
+        const int SaltSize = 16;
+        const int Iterations = 10000;
+        const int HashSize = 32;
+        private readonly HashAlgorithmName HashAlgorithm = HashAlgorithmName.SHA256;
+        private const char stringDelimiter = '.';
+
+        public string HashPassword(string password)
+        {
+            var salt = RandomNumberGenerator.GetBytes(SaltSize);
+            var hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, HashAlgorithm, HashSize);
+
+            return string.Join(stringDelimiter, Convert.ToBase64String(salt), Convert.ToBase64String(hash));
+        }
+
+        public bool VerifyPassword(string password, string storedHash)
+        {
+            var parts = storedHash.Split(stringDelimiter);
+            var salt = Convert.FromBase64String(parts[0]);
+            var hash = Convert.FromBase64String(parts[1]);
+
+            var newHash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, HashAlgorithm, HashSize);
+            return newHash.SequenceEqual(hash);
+        }
+    }
+}
