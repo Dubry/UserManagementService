1750d82aafd17519f71732d0bdf0e2ec4edfe29b
Dubry <dubrywr@gmail.com>
2025-12-13 14:06:08 +0100

    Folder structure User Model API Keys Model

---
+++ UserManagementService/Models/ApiClient.cs
@@ -0,0 +1,11 @@
+﻿namespace UserManagementService.Models
+{
+    public class ApiClient
+    {
+        public int Id { get; set; }
+
+        public string ClientName { get; set; }
+
+        public string ApiKey { get; set; }
+    }
+}
---
+++ UserManagementService/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementService.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; private set; } = Guid.NewGuid();
+
+        [Required]
+        public string UserName { get; set; }
+
+        public string FullName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string MobileNumber { get; set; }
+
+        public string Language {  get; set; }
+
+        public string Culture { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+    }
+}
